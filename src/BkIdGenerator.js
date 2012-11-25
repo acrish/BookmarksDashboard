d342d53f6802f31b93ccf7c469ede14dbdb7a471
andreea <hodea_andreea@yahoo.com>
2012-11-26 01:25:05 +0200

    drag and drop persistent

--- src/BkIdGenerator.js
+++ src/BkIdGenerator.js
@@ -9,6 +9,10 @@ function BkIdGenerator() {
 
 BkIdGenerator.idTemplate = 'bookmark';
 
+/**
+ * Generate the first unused id.
+ * @returns the first unused id
+ */
 BkIdGenerator.getNextId = function() {
 	var i = 0;
 	var currId = BkIdGenerator.idTemplate + i;
@@ -21,6 +25,11 @@ BkIdGenerator.getNextId = function() {
 	return currId;
 };
 
+/**
+ * Generate an id based on the number given as parameter.
+ * @param which suffix of template
+ * @returns id
+ */
 BkIdGenerator.getId = function(which) {
 	
 	return BkIdGenerator.idTemplate + which;
